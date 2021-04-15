7541ddd201cc95fdf8dee11141269fc02b19f988
Maia <maiafleschler@gmail.com>
2021-04-15 14:12:07 -0300

    Segundo commit

--- ej4 - contador/app2.js
+++ ej4 - contador/app2.js
@@ -4,6 +4,8 @@
 //-1, +1, -5, +5, -10, +10. Cuando se clickea un botón, se tiene que sumar o restar la cantidad correspondiente, 
 //y actualizar el número mostrado.
 
+// Hecho con funciones separadas
+
 const botonMenos1 = document.querySelector("#botonMenos1");
 const botonMas1 = document.querySelector("#botonMas1");
 const botonMenos5 = document.querySelector("#botonMenos5");
@@ -11,33 +13,38 @@ const botonMas5 = document.querySelector("#botonMas5");
 const botonMenos10 = document.querySelector("#botonMenos10");
 const botonMas10 = document.querySelector("#botonMas10");
 const numeroHtml = document.querySelector("h1");
+
 let numero = 0;
 
-const restarUno = ()=>{
-   
+const restaUno = ()=>{
+    numero = numero - 1;
+    numeroHtml.textContent = numero;
 }
-
-botonMenos1.addEventListener("click", (event) =>{
-   numero = numero -1;
-   numeroHtml.textContent = numero;
-})
-botonMas1.addEventListener("click", (event) =>{
-    numero = numero +1;
+const SumaUno = ()=>{
+    numero = numero + 1;
     numeroHtml.textContent = numero;
- })
- botonMenos5.addEventListener("click", (event) =>{
-    numero = numero -5;
+}
+const restaCinco = ()=>{
+    numero = numero - 5;
     numeroHtml.textContent = numero;
- })
- botonMas5.addEventListener("click", (event) =>{
+}
+const sumaCinco = ()=>{
     numero = numero +5;
     numeroHtml.textContent = numero;
- })
- botonMenos10.addEventListener("click", (event) =>{
-    numero = numero -10;
+}
+const restaDiez = ()=>{
+    numero = numero - 10;
     numeroHtml.textContent = numero;
- })
- botonMas10.addEventListener("click", (event) =>{
+}
+const sumaDiez = ()=>{
     numero = numero +10;
     numeroHtml.textContent = numero;
- })
+}
+
+botonMenos1.addEventListener("click", restaUno);
+botonMas1.addEventListener("click",SumaUno);
+botonMenos5.addEventListener("click",restaCinco);
+botonMas5.addEventListener("click", sumaCinco);
+botonMenos10.addEventListener("click", restaDiez);
+botonMas10.addEventListener("click", sumaDiez);
+
